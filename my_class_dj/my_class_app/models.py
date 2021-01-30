e213e8e1f060bdf7e6da29690c80f9560d320f42
Julia <julie.panov@gmail.com>
2021-01-31 01:34:18 +0300

    Task 7: authorization using django User model. database removed from VCS, secret key moved to local settings, redundant comments removed

--- my_class_dj/my_class_app/models.py
+++ my_class_dj/my_class_app/models.py
@@ -1,41 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    #id
-    id = models.AutoField(primary_key=True)
-    #login
-    login = models.CharField(max_length=64)
-    #password
-    pswrd = models.CharField(max_length=64)
-    #Name
     name = models.CharField(max_length=64)
-    #Phone number
     phone = models.CharField(max_length=64)
+
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
 
 
 class Teacher(models.Model):
-    # id
-    id = models.AutoField(primary_key=True)
-    # login
-    login = models.CharField(max_length=64)
-    # password
-    pswrd = models.CharField(max_length=64)
-    # Name
     name = models.CharField(max_length=64)
-    # Phone number
-    phone = models.CharField(max_length=64)
+    degree = models.CharField(max_length=64)
+    rate = models.IntegerField()
+
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
 
 
 class Lesson(models.Model):
-    #id
-    id = models.AutoField(primary_key=True)
     student = models.ForeignKey(Student, on_delete=models.PROTECT)
     teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT)
     #Date
